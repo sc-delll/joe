93d42676ecad7c778bc186057b1a92b6ea7aad8e
sc-delll <82901274+sc-delll@users.noreply.github.com>
2021-10-30 21:43:48 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 window.confirm("audio ?");
 if (confirm("Press ok to comfirm audio ")) {
-  txt = "Ok here it comes";
+  txt = music() "Ok here it comes";
 } else {
   txt = "You dont like sound?";
 }
+function music(){
+  var music = new Audio('loud indian music.mp3');
+ music.play();
